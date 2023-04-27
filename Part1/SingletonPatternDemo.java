db21c76c22bf1d9658458fb137319416c0dfcc1a
mikemoreedu <81551332+mikemoreedu@users.noreply.github.com>
2023-04-26 21:50:58 -0400

    Add files via upload

---
+++ Part1/SingletonPatternDemo.java
@@ -0,0 +1,9 @@
+package Part1;
+
+public class SingletonPatternDemo {
+	public static void main(String[] args) {
+		SingleObject object = SingleObject.getInstance();
+		object.showMessage();
+	}
+
+}
