5aa6fe7ee0daa315bc47cb73a68d2bef0fafb322
mikemoreedu <81551332+mikemoreedu@users.noreply.github.com>
2023-04-26 21:50:13 -0400

    Add files via upload

--- Part2/ComputerPart.java
+++ Part2/ComputerPart.java
@@ -1,1 +1,6 @@
+package test;
 
+public interface ComputerPart {
+	public void accept(ComputerPartVisitor computerPartVisitor);
+
+}
---
+++ Part2/Mouse.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Mouse implements ComputerPart{
+
+	@Override
+	public void accept(ComputerPartVisitor computerPartVisitor) {
+		computerPartVisitor.visit(this);
+}
+}
